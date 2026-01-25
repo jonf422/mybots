c2a8cd90850edd6be0883091f0c117a3d00458ed
jonf422 <jonf422@gmail.com>
2026-01-25 10:11:22 -0500

    changed dimensions of box

--- generate.py
+++ generate.py
@@ -1,5 +1,9 @@
 import pyrosim.pyrosim as ps
 
+length=1
+width=2
+height=3
+
 ps.Start_SDF("box.sdf")
-ps.Send_Cube(name="Box", pos=[0,0,.5], size=[1,1,1])
+ps.Send_Cube(name="Box", pos=[0,0,.5], size=[length,width,height])
 ps.End()
