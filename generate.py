fca0b1879d8b7a9d55eddb08de6be1a92b492d96
jonf422 <jonf422@gmail.com>
2026-01-25 10:36:31 -0500

    created 3d array of boxes

--- generate.py
+++ generate.py
@@ -3,9 +3,14 @@ import pyrosim.pyrosim as ps
 length,width,height = 1,1,1
 x,y,z = 0,0,.5
 
+rows, cols = 5,5
 
 ps.Start_SDF("boxes.sdf")
 
-for i in range(10):
-    ps.Send_Cube(name="Box"+str(i), pos=[x,y,z+i], size=[length-.1*i,width-.1*i,height-.1*i])
+for r in range(rows):
+    for c in range(cols):
+        for i in range(10):
+            ps.Send_Cube(name="Box"+str(r)+str(c)+str(i), 
+                         pos=[x+r,y+c,z+i], 
+                         size=[length-.1*i,width-.1*i,height-.1*i])
 ps.End()
