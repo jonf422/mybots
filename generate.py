fce21f80c62e1a21c32e097fad85402e4ba58e38
jonf422 <jonf422@gmail.com>
2026-01-25 10:30:22 -0500

    generated block stack with decremented size

--- generate.py
+++ generate.py
@@ -5,6 +5,7 @@ x,y,z = 0,0,.5
 
 
 ps.Start_SDF("boxes.sdf")
-ps.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
-ps.Send_Cube(name="Box2", pos=[x+1,y,z+1], size=[length,width,height])
+
+for i in range(10):
+    ps.Send_Cube(name="Box"+str(i), pos=[x,y,z+i], size=[length-.1*i,width-.1*i,height-.1*i])
 ps.End()
