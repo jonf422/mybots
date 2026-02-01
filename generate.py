0481e3be6cd9ae26fa061ce6785b6d7c2fcf8300
jonf422 <jonf422@gmail.com>
2026-02-01 16:36:43 -0500

    Created World

--- generate.py
+++ generate.py
@@ -5,12 +5,7 @@ x,y,z = 0,0,.5
 
 rows, cols = 5,5
 
-ps.Start_SDF("boxes.sdf")
+ps.Start_SDF("world.sdf")
 
-for r in range(rows):
-    for c in range(cols):
-        for i in range(10):
-            ps.Send_Cube(name="Box"+str(r)+str(c)+str(i), 
-                         pos=[x+r,y+c,z+i], 
-                         size=[length-.1*i,width-.1*i,height-.1*i])
+ps.Send_Cube(name="Box",pos=[x,y,z], size=[length,width,height])
 ps.End()
