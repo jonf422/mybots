34e275347261dd55b253652be1647aac31caf03e
jonf422 <jonf422@gmail.com>
2026-02-08 15:49:03 -0500

    Plot both sensors

--- analyze.py
+++ analyze.py
@@ -1,5 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-load = np.load("data/backLegSensorValues.npy")
-print(load)
+backLeg = np.load("data/backLegSensorValues.npy")
+frontLeg = np.load("data/frontLegSensorValues.npy")
+
+
+plt.plot(backLeg, color="red", linewidth=3)
+plt.plot(frontLeg, color="blue", linewidth=3)
+plt.legend()
+plt.show()
