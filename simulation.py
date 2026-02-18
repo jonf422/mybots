0fbebd01f0f351f24bfa81d9c7736f488528b436
jonf422 <jonf422@gmail.com>
2026-02-17 20:21:48 -0500

    Robot moves forwards

--- simulation.py
+++ simulation.py
@@ -3,6 +3,17 @@ import pybullet_data
 import pyrosim.pyrosim as ps
 import numpy as np
 import time
+import random
+
+#VARIABLES
+fl_amplitude = np.pi/4
+fl_frequency = .5
+fl_phaseShift = 0
+
+bl_amplitude = np.pi/8
+bl_frequency = 10
+bl_phaseShift = 0
+
 
 physicsClient = pb.connect(pb.GUI) #set up pybullet
 pb.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -18,6 +29,13 @@ ps.Prepare_To_Simulate(robotId) #prepare pyrosim for sensor simulation
 backLegSensorValues = np.zeros(1000)
 frontLegSensorValues = np.zeros(1000)
 
+x = np.linspace(0, 2*np.pi, 1000)
+fl_targetAngles = fl_amplitude * np.sin(fl_frequency*x + fl_phaseShift)
+bl_targetAngles = bl_amplitude * np.sin(bl_frequency*x + bl_phaseShift)
+
+#np.save("data/fl_targetAngles", fl_targetAngles, True)
+#np.save("data/bl_targetAngles", bl_targetAngles, True)
+#exit()
 
 for i in range(1000): #simulate for 5000 iterations
     pb.stepSimulation()
@@ -25,7 +43,8 @@ for i in range(1000): #simulate for 5000 iterations
     backLegSensorValues[i] = ps.Get_Touch_Sensor_Value_For_Link("BackLeg") #get touch sensor data for front and back leg
     frontLegSensorValues[i] = ps.Get_Touch_Sensor_Value_For_Link("FrontLeg")
 
-    ps.Set_Motor_For_Joint(bodyIndex = robotId, jointName = b'Torso_BackLeg', controlMode = pb.POSITION_CONTROL, targetPosition = -np.pi/4, maxForce = 500)
+    ps.Set_Motor_For_Joint(bodyIndex = robotId, jointName = b'Torso_BackLeg', controlMode = pb.POSITION_CONTROL, targetPosition = bl_targetAngles[i], maxForce = 500)
+    ps.Set_Motor_For_Joint(bodyIndex = robotId, jointName = b'Torso_FrontLeg', controlMode = pb.POSITION_CONTROL, targetPosition = fl_targetAngles[i], maxForce = 500)
     
     time.sleep(1/60)
     #print(i) #print iteration
