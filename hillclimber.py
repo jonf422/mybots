79f7378e1ffa5c08e4109d60bd257635e131d667
jonf422 <jonf422@gmail.com>
2026-03-24 10:51:04 -0400

    created random 3x2 solution

---
+++ hillclimber.py
@@ -0,0 +1,7 @@
+import solution
+
+class HILL_CLIMBER:
+
+    def __init__(self):
+        self.parent = solution.SOLUTION()
+
--- search.py
+++ search.py
@@ -1,5 +1,8 @@
 import os
+import hillclimber
 
-for i in range(5):
-    os.system("python generate.py")
-    os.system("python simulate.py")
+#for i in range(5):
+#    os.system("python generate.py")
+#    os.system("python simulate.py")
+
+hc = hillclimber.HILL_CLIMBER()
